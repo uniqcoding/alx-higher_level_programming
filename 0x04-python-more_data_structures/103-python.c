f571eff5761f254a9435ce74b530e82f7f3833c1
Meli <uniqcoding@gmail.com>
2022-12-14 02:28:37 -0800

    Python basic info

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,59 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+
+/**
+ * print_python_bytes - Prints basic info about Python byte objects.
+ * @p: Python object
+ */
+
+void print_python_bytes(PyObject *p)
+{
+long int size;
+int i;
+char *trying_str = NULL;
+
+printf("[.] bytes object info\n");
+if (!PyBytes_Check(p))
+{
+printf(" [ERROR] invalid Bytes Object\n");
+return;
+}
+
+PyBytes_AsStringAndSize(p, &tring_str, &size);
+
+printf(" size: %li\n", size);
+printf(" trying string: %s\n", trying_str);
+if (size < 10)
+	printf(" first 5li bytes:", size + 1);
+else
+	printf(" first 10 bytes:");
+for (i = 0; i <= size && i < 10; i++)
+	printf(" %02hhx", trying_str[i]);
+printf("\n");
+}
+
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: Python object
+ */
+
+void print_python_list(PyObject *p)
+{
+long int size = PyList_Size(p);
+int i;
+PyListObject *list = (PyListObject *)p;
+const char *type;
+
+printf("[*] Python list infor\n");
+printf("[*] Size of the Python List = %li\n", size);
+printf("[*] Allocated = %li\n", list->allocated);
+for (i = 0; i < size; i++)
+{
+type = (list->ob_item[i]->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
+		if (!strcmp(type, "bytes"))
+		print_python_bytes(list->ob_item[i]);
+}
+}
