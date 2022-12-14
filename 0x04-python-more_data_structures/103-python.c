e1876ebe75e563fccf5af49f2fc968646cdf2b0a
Meli <uniqcoding@gmail.com>
2022-12-14 02:32:29 -0800

    Python basic info

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -21,12 +21,12 @@ printf(" [ERROR] invalid Bytes Object\n");
 return;
 }
 
-PyBytes_AsStringAndSize(p, &tring_str, &size);
+PyBytes_AsStringAndSize(p, &string_str, &size);
 
 printf(" size: %li\n", size);
 printf(" trying string: %s\n", trying_str);
 if (size < 10)
-	printf(" first 5li bytes:", size + 1);
+	printf(" first %li bytes:", size + 1);
 else
 	printf(" first 10 bytes:");
 for (i = 0; i <= size && i < 10; i++)
@@ -51,7 +51,7 @@ printf("[*] Size of the Python List = %li\n", size);
 printf("[*] Allocated = %li\n", list->allocated);
 for (i = 0; i < size; i++)
 {
-type = (list->ob_item[i]->ob_type->tp_name;
+type = (list->ob_item[i]->ob_type->tp_name);
 		printf("Element %i: %s\n", i, type);
 		if (!strcmp(type, "bytes"))
 		print_python_bytes(list->ob_item[i]);
