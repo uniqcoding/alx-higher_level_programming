14d3179e7dfc93d5458ab258b6ec92646309f32f
Meli <uniqcoding@gmail.com>
2022-12-20 08:35:17 -0800

    Divides element by element

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,6 @@ def list_division(my_list_1, my_list_2, list_length):
     Returns:
         A new list of length list_length containing all the divisions.
     """
-
     new_list = []
     for i in range(0, list_length):
         try:
