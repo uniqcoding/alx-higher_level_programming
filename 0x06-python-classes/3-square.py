05d08be8a16deb434e76f3245b1da501ecd0b279
Meli <uniqcoding@gmail.com>
2023-01-09 06:39:35 -0800

    Rep square1

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,6 +2,7 @@
 
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
@@ -17,6 +18,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """Return the current area of the square."""
-            return (self.__size * self.__size)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
