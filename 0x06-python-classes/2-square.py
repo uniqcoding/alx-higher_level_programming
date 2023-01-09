bdc70716d4a6e5c2668a7f36dcbf4cce8166ad89
Meli <uniqcoding@gmail.com>
2023-01-09 06:29:47 -0800

    Rep a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,6 +2,7 @@
 
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
@@ -12,7 +13,7 @@ class Square:
             size (int): The size of the new square.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer.")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
