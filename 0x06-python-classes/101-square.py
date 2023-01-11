51961109e07701c286df1a4ed6ca62eba0669617
Meli <uniqcoding@gmail.com>
2023-01-11 05:01:11 -0800

    Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -41,7 +41,7 @@ class Square:
                 not all(isinstance(num, int) for
 num in value) or
                 not all(num >= 0 for num in value)):
-            raise TypeError("position must ne a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
@@ -59,13 +59,14 @@ num in value) or
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+
     def __str__(self):
         """Define the print() representation of a square."""
         if self.__size != 0:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self._size)]
+            [print("#", end="") for k in range(0, self.__size)]
             if i != self.__size - 1:
                 print("")
             return ("")
